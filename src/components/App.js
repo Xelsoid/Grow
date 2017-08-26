d27223cd50adca6f1305d09cbcf181256a8817e4
Dzmitry <Dzmitry_Kudla@epam.com>
2017-08-26 08:11:32 +0000

    ee-grow-yourself/feature: edited App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Profile from './Profile.jsx';
-import Signup from './Signup.jsx';
-import Login from './Login.jsx';
+import Profile from './Profile';
+import Signup from './Signup';
+import Login from './Login';
 
 class App extends Component {
   render() {
